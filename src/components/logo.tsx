69d701ee7e4c40f92e36dc9ca115c104be031441
kopenkinda <kopenkin.da@gmail.com>
2022-07-26 01:38:01 +0200

    react -> astro rewrite

--- src/components/logo.tsx
+++
@@ -1,7 +0,0 @@
-export default function Logo() {
-  return (
-    <h1 className="tx-md bg-gradient-to-br from-pink-400 to-fuchsia-600 bg-clip-text font-extrabold text-transparent lg:text-2xl">
-      Kopenkin Dmitrii
-    </h1>
-  );
-}
--- src/components/ui/under-construction.tsx
+++
@@ -1,7 +0,0 @@
-export default function UnderConstruction() {
-  return (
-    <div className="inline-flex animate-pulse rounded-md border border-zinc-300 p-4">
-      <strong className="text-gray-700">🔨 Under construction</strong>
-    </div>
-  );
-}
