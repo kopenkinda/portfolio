4425ba73cb292a78800e7db0e87cf0c73ac2c6dc
kopenkinda <kopenkin.da@gmail.com>
2022-07-26 02:30:53 +0200

    Added @astrojs/image (possible revert)

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,18 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import prefetch from "@astrojs/prefetch";
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://kopenkin.tech",
-  integrations: [react(), tailwind(), sitemap(), prefetch()]
+  integrations: [
+    react(),
+    tailwind(),
+    sitemap(),
+    prefetch(),
+    // @ts-expect-error
+    image({}),
+  ],
 });
