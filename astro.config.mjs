73bc419165e519f479faff5744e2f7d2baf15516
Dmitrii Kopenkin s-dkopenkin@digikare.com <s-dkopenkin@digikare.com>
2023-09-07 23:03:27 +0200

    feat: sitemap & prefetch + port old projects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import prefetch from "@astrojs/prefetch";
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), prefetch(), sitemap()],
+  site: "https://kopenkin.tech",
 });
