0ecccfdbe6f05195693bb3d804bbcdee67a24d8a
Dmitrii Kopenkin s-dkopenkin@digikare.com <s-dkopenkin@digikare.com>
2023-09-08 19:20:03 +0200

    feat(gamedle-assistant): design improvements

--- src/components/interactive/gamedle/gamedle-list.tsx
+++ src/components/interactive/gamedle/gamedle-list.tsx
@@ -7,13 +7,7 @@ import { useFilters } from "./filters.store";
 import GameItem from "./game-item";
 import { Button } from "../Button";
 
-const GamedleList = ({
-  initial,
-  lastUpdated,
-}: {
-  initial: Game[];
-  lastUpdated: Date;
-}) => {
+const GamedleList = ({ initial }: { initial: Game[] }) => {
   const filters = useFilters();
   const [showingRev, setShowingRev] = useState(-1);
   const [loading, setLoading] = useState(false);
@@ -35,23 +29,12 @@ const GamedleList = ({
   return (
     <div className="my-4 w-full space-y-2">
       <Filters />
-      <div className="w-full">
-        <p className="mt-12 text-center">
-          <span className="font-bold">{filtered.length}</span> games found |
-          Last update:{" "}
-          <span className="font-bold">
-            {lastUpdated.toLocaleDateString("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "numeric",
-            })}
-          </span>
-        </p>
+      <div className="w-full pt-8">
         {filters.rev !== showingRev ? (
           <div className="mt-2 flex items-center justify-center gap-2">
             {filtered.length !== 0 && (
               <Button className="px-2 pl-3" onClick={showList}>
-                Show results
+                Show {filtered.length} results
                 {loading ? (
                   <IconLoader stroke={1} className="animate-spin" />
                 ) : (
