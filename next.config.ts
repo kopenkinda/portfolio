898d4b0903ad1daa3c09269d732e47acd9aedc47
Kopenkin Dmitrii <dmitrii@moveworkforward.com>
2025-12-12 11:31:56 +0100

    fix CVE

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  experimental: {
-    reactCompiler: true,
-  },
   output: "standalone",
 };
 
