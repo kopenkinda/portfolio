f684e4e1c2f66272c92e268bbab44c03add87415
kopenkinda <kopenkin.da@gmail.com>
2026-05-04 18:17:40 +0200

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,47 +1,47 @@
 // @ts-check
 
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
-import { defineConfig, fontProviders } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig, fontProviders } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: "https://example.com",
   integrations: [mdx(), sitemap()],
 
   fonts: [
     {
       provider: fontProviders.local(),
-      name: 'Atkinson',
-      cssVariable: '--font-atkinson',
-      fallbacks: ['sans-serif'],
+      name: "Atkinson",
+      cssVariable: "--font-atkinson",
+      fallbacks: ["sans-serif"],
       options: {
         variants: [
           {
-            src: ['./src/assets/fonts/atkinson-regular.woff'],
+            src: ["./src/assets/fonts/atkinson-regular.woff"],
             weight: 400,
-            style: 'normal',
-            display: 'swap',
+            style: "normal",
+            display: "swap",
           },
           {
-            src: ['./src/assets/fonts/atkinson-bold.woff'],
+            src: ["./src/assets/fonts/atkinson-bold.woff"],
             weight: 700,
-            style: 'normal',
-            display: 'swap',
+            style: "normal",
+            display: "swap",
           },
         ],
       },
     },
     {
       provider: fontProviders.google(),
-      name: 'Doto',
-      cssVariable: '--font-doto',
-      weights: ['100 900'],
-      styles: ['normal'],
-      subsets: ['latin'],
-      fallbacks: ['sans-serif'],
+      name: "Doto",
+      cssVariable: "--font-doto",
+      weights: ["100 900"],
+      styles: ["normal"],
+      subsets: ["latin"],
+      fallbacks: ["sans-serif"],
     },
   ],
 
