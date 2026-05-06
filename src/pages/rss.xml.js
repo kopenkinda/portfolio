d6b81859d8b99fe654fc2efe7a16bb9d4b2c598f
kopenkinda <kopenkin.da@gmail.com>
2026-05-06 14:58:17 +0200

    Migrate RSS feed to use CONFIG object
    - Switch from SITE_TITLE/SITE_DESCRIPTION to CONFIG.meta
    - Centralizes site configuration

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,12 +1,12 @@
 import { getCollection } from "astro:content";
 import rss from "@astrojs/rss";
-import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
+import { CONFIG } from "../consts";
 
 export async function GET(context) {
   const posts = await getCollection("blog");
   return rss({
-    title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    title: CONFIG.meta.title,
+    description: CONFIG.meta.description,
     site: context.site,
     items: posts.map((post) => ({
       ...post.data,
