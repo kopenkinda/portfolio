be4731e345e8b1b3e51230e2f76a835f19812c38
kopenkinda <kopenkin.da@gmail.com>
2026-05-06 04:07:25 +0200

    Move experiences data to consts.ts
    - Consolidate experience entries into USER object
    - Update ExperienceTimeline to use new data structure

--- src/consts.ts
+++ src/consts.ts
@@ -1,12 +1,16 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 import type { AllowedPixelIconType } from "./components/pixel-icon.types";
+import type { TechId } from "./data/technologies";
 import andorraPhoto from "./assets/globe/andorra.jpg";
 import yekaterinburgPhoto from "./assets/globe/yekaterinburg.jpg";
 
 export const SITE_TITLE = "Kopenkin Dmitrii | Full-Stack AI Developer";
 export const SITE_DESCRIPTION = "Welcome to my website!";
 export const STARTED_DEVELOPMENT_IN = new Date("2017-01-01");
+
+let id = 0;
+const iota = () => id++;
 
 export const USER = {
   name: "Kopenkin Dmitrii",
@@ -72,6 +76,161 @@ export const USER = {
       },
     ] satisfies MapConnection[],
   },
+  experiences: {
+    theBeginning:
+      "Deployed into the world with a sweet tooth and a chef's ambition. Kept getting syntax errors in my sauces and failed builds on the line. Debugged my trajectory, swapped ingredients for APIs, and now I craft full-stack dishes that never burn—turns out my real calling was sautéing state, not steak.",
+    entries: [
+      {
+        id: iota(),
+        title: "CTO",
+        startDate: new Date("2026-01-01"),
+        company: {
+          location: "Remote / Spain",
+          name: "Salut Mercado",
+          shortname: "Salut",
+          href: "https://salutmercado.com",
+        },
+        bulletpoints: ["Lead the development of a new product from scratch."],
+        technologies: [
+          "openai",
+          "typescript",
+          "react",
+          "supabase",
+          "python",
+          "fastapi",
+          "escpos",
+        ],
+      },
+      {
+        id: iota(),
+        title: "Software Engineer",
+        startDate: new Date("2024-12-15"),
+        endDate: new Date("2025-12-21"),
+        company: {
+          location: "Remote",
+          name: "MoveWorkForward",
+          shortname: "MWF",
+          href: "https://moveworkforward.com",
+        },
+        bulletpoints: [
+          "Prototyped PoC's to data security and privacy solutions for AI features in multi-tenant environments.",
+          "Maintained multiple applications using React, Typescript, Node.js, and AWS.",
+          "Participated in mutiple conferences/workshops leading to the creation and ownership of a new product.",
+        ],
+        technologies: ["react", "typescript", "openai", "aws", "nodejs"],
+      },
+      {
+        id: iota(),
+        title: "Fullstack developer",
+        startDate: new Date("2024-10-31"),
+        endDate: new Date("2024-12-15"),
+        company: {
+          location: "Remote / Andorra",
+          name: "Freelance",
+          shortname: "Freelance",
+        },
+        bulletpoints: [
+          "Created a custom trading bot for a client, integrated into QUIK, with a dashboard for monitoring and managing trades.",
+        ],
+        technologies: ["hono", "lua", "react", "typescript", "sqlite"],
+      },
+      {
+        id: iota(),
+        company: {
+          name: "Digikare",
+          location: "Remote / Toulouse, France",
+          shortname: "DGK",
+          href: "https://digikare.com",
+        },
+        startDate: new Date("2023-08-01"),
+        endDate: new Date("2024-10-31"),
+        title: "Fullstack Developer",
+        bulletpoints: [
+          "Developed three full stack AI-enhanced applications. Integrated external systems, machine learning, and improved performance by 20% through DDD, CQRS, and API integrations.",
+        ],
+        technologies: ["typescript", "angular", "nodejs", "nestjs", "mongodb"],
+      },
+      {
+        id: iota(),
+        company: {
+          name: "Toulouse Ynov Campus",
+          shortname: "TYC",
+          location: "On-Site / Toulouse, France",
+          href: "https://www.ynov.com/campus/toulouse/",
+        },
+        startDate: new Date("2021-05-01"),
+        endDate: new Date("2023-08-01"),
+        title: "Senior Mentor",
+        bulletpoints: [
+          "Mentored over 150 first and second year students, providing guidance and support throughout their study program. Created and graded exams for multiple courses, ensuring accuracy and fairness in evaluation processes leading to an average exam score improvement of 15% among students.",
+        ],
+      },
+      {
+        id: iota(),
+        company: {
+          name: "Toulouse Ynov Campus",
+          href: "https://www.ynov.com/campus/toulouse/",
+          shortname: "TYC",
+          location: "Hybrid / Toulouse, France",
+        },
+        startDate: new Date("2023-01-01"),
+        endDate: new Date("2023-05-01"),
+        title: "Lead Full-Stack Developer",
+        bulletpoints: [
+          'Developed a job "dating" organization platform for students and companies. Integrated a chat system, user profiles, and a job board.',
+          "Organized, managed and led the development of a CTF (Capture The Flag) event for students. Created a platform for challenges and scoring. Assembled and managed a team of 5 developers, insuring on-time delivery of the platform.",
+        ],
+        technologies: [
+          "typescript",
+          "astro",
+          "githubactions",
+          "sanity",
+          "tailwindcss",
+          "postgres",
+        ],
+      },
+      {
+        id: iota(),
+        company: {
+          name: "Hospitalidee",
+          shortname: "HID",
+          href: "https://hospitalidee.fr",
+          location: "Remote / Paris, France",
+        },
+        startDate: new Date("2020-09-01"),
+        endDate: new Date("2021-05-01"),
+        title: "Full-stack Intern",
+        technologies: ["react", "nodejs", "express", "mongodb"],
+        bulletpoints: [
+          "Optimized the search flow on the platform through database restructuring and algorithms, resulting in a 400% improvement in search performance.",
+        ],
+      },
+      {
+        id: iota(),
+        company: {
+          location: "Remote / Russia",
+          name: undefined,
+          shortname: undefined,
+          href: undefined,
+        },
+        startDate: new Date("2016-09-01"),
+        endDate: new Date("2020-09-01"),
+        bulletpoints: [
+          "Implemented custom seamless integration between Wordpress and IIKO systems to streamline online ordering processes, resulting in a 20% increase in customer orders.",
+          "Developed multiple static and dynamic websites for clients, including e-commerce platforms and blogs.",
+        ],
+        title: "Freelancer",
+        technologies: [
+          "gulp",
+          "react",
+          "php",
+          "javascript",
+          "mysql",
+          "wordpress",
+        ],
+      },
+    ] satisfies ExperienceEntry[],
+  },
 };
 
 type SocialLink = {
@@ -97,4 +256,21 @@ type MapConnection = {
   id: string;
   label?: string;
   color?: [number, number, number];
+};
+
+type ExperienceEntry = {
+  id: number;
+  startDate: Date;
+  endDate?: Date;
+  title: string;
+  company:
+    | { location: string; name: string; shortname: string; href?: string }
+    | {
+        location: string;
+        name: undefined;
+        shortname: undefined;
+        href: undefined;
+      };
+  bulletpoints: [string] | [string, string] | [string, string, string];
+  technologies?: TechId[];
 };
